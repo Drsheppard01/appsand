7afa1e1e71466bf7ffeb3808d7139388ca3e4fc0
drsheppard01 <gbog@proton.me>
2024-02-14 02:56:11 +0300

    add appsand.sh

---
+++ appsand.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+### 1. Download Alpinerotfs (we wanna find more simple and less) Ham
+### 2. Include your content in Cheese using constants
+### 3. Make desktop integration (Lettuce level)
+### 4. Make Image using mksquashfs
+
+must_run_as_root() {
+	[ "$(id -u)" -eq 0 ] || die 100 'must be run as root'
+}
+
+LINK="https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-minirootfs-3.19.1-x86_64.tar.gz"
+NAME_RUNTIME="alpine-minirootfs-3.19.1-x86_64"
+
+mkdir My-Roast-Project
+cd My-Roast-Project || return
+
+if [ -n "curl" ]; then
+curl $LINK > $NAME_RUNTIME
+  elif
+  [ -n "wget" ]; then
+wget $LINK
+  elif
+  [ -n "aria2c" ]; then
+aria2c $LINK
+fi
+
+if [ -n "dnf" ]; then
+dnf install squashfs-tools
+  elif
+  [ -n "apt" ]; then
+apt install squashfs-tools
+  elif
+  [ -n "apk" ]; then
+apk add squashfs-tools
+ elif
+  [ -n "pacman" ]; then
+pacman -S squashfs-tools
+ elif
+  [ -n "zypper" ]; then
+zypper in squashfs-tools
+ elif
+  [ -n "emerge" ]; then
+emerge --ask sys-fs/squashfs-tools
+ fi
